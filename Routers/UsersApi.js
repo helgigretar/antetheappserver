8e97e7362f1933a383a69e1b2663fd1b1d58b10d
Helgi Grétar Gunnarsson <helgigretar@icelandair.is>
2021-08-02 14:12:32 +0000

    remove sql stuff

--- Routers/UsersApi.js
+++ Routers/UsersApi.js
@@ -33,7 +33,8 @@ async function registerUser(req) {
 //Login returns status strue if user may login
 router.post('/userLogin', async function (req, res) {
     const { user_name, password } = req.body;
-    const cred = global.credentials
+    return res.json({ "user": user, "status": true })
+    /*const cred = global.credentials
     const client = new Client({ user: cred.user, host: cred.host, database: cred.database, password: cred.password, port: 5432 });
     await client.connect()
     const query = "Select id,name,user_name,password,email,country,age,image_url,description,gender from users Where user_name = $1 and password = $2";
@@ -53,7 +54,7 @@ router.post('/userLogin', async function (req, res) {
         return res.json({ "user": user, "status": true })
     } else {
         return res.json({ "status": false })
-    }
+    }*/
 })
 
 // Create users
