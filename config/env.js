94d8d496b0013aaf60127f1c076dd62560125f2f
Helgi Grétar Gunnarsson <helgigretar@icelandair.is>
2021-08-02 14:29:40 +0000

    change host name

--- config/env.js
+++ config/env.js
@@ -9,7 +9,7 @@ function getCredentials() {
     //return obj;
      const obj = {
        "user":"drejoxqksziety",
-       "host":"ec2-54-195-247-108.eu-west-1.compute.amazonaws.com",
+       "host":"ec2-54-75-84-38.eu-west-1.compute.amazonaws.com",
        "database":"d711qi31b5sskh",
        "password":"c1d0a5ba1ad75f9e465c78ea4222e2f5c83fda8184415a77e38100ce5feac47c",
        "port":5432
